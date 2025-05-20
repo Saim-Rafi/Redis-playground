e374967900473777bdf5f060471d9d85b956672e
Saim Rafi <143498300+Saim-Rafi@users.noreply.github.com>
2025-05-20 23:50:35 +0530

    some practical applications

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const axios = require('axios').default;
+const client = require('./client');
 
 const app = express();
 
 app.get('/',async(req,res)=>{
+    const cachedValue = await client.get('todos');
+    if (cachedValue) return res.json(JSON.parse(cachedValue));
+
     const {data}= await axios.get('https://jsonplaceholder.typicode.com/todos');
+    
+    await client.set('todos',JSON.stringify(data));
+    await client.expire('todos',40);
     return res.json(data); 
 })
 app.listen(9000);
